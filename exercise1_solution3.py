dd51d5e636ede1a9d50ba6c363c462a6a6dc3f9c
David Gutierrez <david@pythonsoftware.solutions>
2019-10-17 14:28:42 -0400

    Added details

--- exercise1_solution3.py
+++ exercise1_solution3.py
@@ -3,7 +3,7 @@ import traceback
 def count_multiples(a, b):
     traceback.print_stack()
     if b % a: # base case will look when b%a is not zero 
-        breakpoint()
+        #breakpoint()
         return 0
     return 1 + count_multiples(a, b / a)  # recursive case .Every time it is zero , we will have a recursive case
 
