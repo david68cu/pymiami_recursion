15f9e37327690ca8f2d8c7489d17eccdf95073fb
David Gutierrez <david@pythonsoftware.solutions>
2020-07-06 17:30:18 -0400

    fixed path to file

--- tests/test_pycodestyle_check.py
+++ tests/test_pycodestyle_check.py
@@ -4,14 +4,14 @@ import sys
 
 # We need to pass the path to each file we want our linter to check 
 # during this test
-path_to_file = sys.path[1]+'/appCode/exercise1_solution.py'
+path_to_file = '../appCode/exercise1_solution.py'
 
 
 def test_conformance():
     """Test that we conform to PEP-8."""
     style = pycodestyle.StyleGuide(quiet=True)
     result = style.check_files([path_to_file])
-    # print(result.print_statistics())
+    print(result.print_statistics())
     assert(result.total_errors == 0)
 
 
